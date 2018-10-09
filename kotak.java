306ed0ec61cf036b5e2a4efa27f8543a58e22ee5
aminhidayatt <43781728+aminhidayatt@users.noreply.github.com>
2018-10-09 21:41:19 +0700

    Create kotak.java

---
+++ kotak.java
@@ -0,0 +1,37 @@
+public class kotak {
+	
+
+
+		private double panjang,lebar,luas,keliling;
+			//method getter
+			public double getpanjang(){
+				return panjang=panjang;
+			}
+			public double getlebar(){
+				return lebar=lebar;
+			}
+			public double getluas(){
+				return luas;
+			}
+			public double getkeliling(){
+				keliling=panjang+lebar+panjang+lebar;
+				return keliling;
+			}
+			//method setter
+			public void setpanjang(double panjang){
+				this.panjang=panjang;
+			}
+			public void setlebar(double lebar){
+				this.lebar=lebar;
+			}
+			public void setluas(double luas){
+				this.luas=luas;
+			}
+			public void setkeliling(double sembarangkeliling){
+				this.keliling=sembarangkeliling;
+			}
+		
+		
+
+	
+}
