f8ee7fdbf3c483bddcf8981ae7a46009db9f7553
aminhidayatt <43781728+aminhidayatt@users.noreply.github.com>
2018-10-09 21:44:50 +0700

    Create node.java

---
+++ node.java
@@ -0,0 +1,22 @@
+public class node {
+	
+	private String label;
+	private int value;
+	//method setter
+	public void setlabel(String label){
+		this.label=label;
+	}
+	public void setvalue(int value){
+		this.value=value;
+	}
+	//method getter
+	public String getlabel(){
+		return label;
+	}
+	public int getvalue(){
+		return value;
+	}
+	
+	
+
+}
